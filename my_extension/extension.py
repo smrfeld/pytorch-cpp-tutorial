347c0417df020d45c0a50f13dfc9358e7b47e7f1
Oliver K. Ernst <oliver.k.ernst@gmail.com>
2023-12-03 23:40:23 -0800

    Removed not needed wrapper

--- my_extension/extension.py
+++
@@ -1,8 +0,0 @@
-import torch
-
-# Load the extension
-my_extension_cpp = torch.ops.load_library("cpp_extension.so")
-
-# Define a wrapper function
-def add_tensors(a, b):
-    return my_extension_cpp.add_tensors(a, b)
